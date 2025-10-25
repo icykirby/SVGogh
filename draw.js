7926d85d4d2e05605b9cc01ae52285afb3f24c3d
icykirby <jamericanmadison@gmail.com>
2025-10-25 00:15:59 -0400

    added svg path tracing and a download button

--- draw.js
+++ draw.js
@@ -15,6 +15,7 @@ canvas.addEventListener("mousedown", (e) => {
     let y = e.offsetY;
     ctx.beginPath();
     ctx.moveTo(x, y);
+    trackSvgPath("M", x, y);
 });
 
 canvas.addEventListener("mousemove", (e) => {
@@ -24,6 +25,7 @@ canvas.addEventListener("mousemove", (e) => {
     if(drawing){
         ctx.lineTo(x, y);
         ctx.stroke();
+        trackSvgPath("L", x, y);
     }
 });
 
@@ -33,4 +35,26 @@ canvas.addEventListener("mouseup", (e) => {
 
 canvas.addEventListener("mouseleave", (e) => {
     drawing = false;
+});
+
+//SVG tracing function
+let svg = "";
+function trackSvgPath(key, x, y){
+    svg += `${key}${x} ${y} `;
+    console.log(svg);
+}
+
+//SVG download button
+const downloadBtn = document.getElementById("download");
+downloadBtn.addEventListener("click", () => {
+    let svgTag = ' width="400px" height="400px" xmlns="http://www.w3.org/2000/svg"';
+    let finalSvg = `<svg ${svgTag}> <path d="${svg}" stroke-width="10" stroke="black" stroke-linecap="round" fill="none"/></svg>`;
+    const blob = new Blob([finalSvg], {type: 'image/svg+xml'});
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = 'text.svg';
+    a.click();
+    URL.revokeObjectURL(a.href);
 });
