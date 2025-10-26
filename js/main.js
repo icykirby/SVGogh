1cfa164c8f2e1436147348650fc5911cf6b25069
Nicole Esposito <79225981+nicoleesposito@users.noreply.github.com>
2025-10-26 10:31:33 -0400

    Update main.js removed stats

--- js/main.js
+++ js/main.js
@@ -50,26 +50,6 @@ particlesJS("particles-js", {
     "retina_detect": true
 });
 
-// Initialize stats.js
-var stats = new Stats();
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-
-var count_particles = document.querySelector('.js-count-particles');
-
-function update() {
-    stats.begin();
-    stats.end();
-    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-        count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-    }
-    requestAnimationFrame(update);
-}
-
-requestAnimationFrame(update);
 
 particlesJS("particles-js", {
     "particles": {
