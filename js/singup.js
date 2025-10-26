2d0538bbbc54c7c9ee977901f470aa9b0fdbba58
Hakim <162640038+Hakim-Abdurrashid@users.noreply.github.com>
2025-10-26 02:25:08 -0400

    Update singup.js

--- js/singup.js
+++ js/singup.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://svgogh.onrender.com/";
+const API_BASE_URL = "https://svgogh.onrender.com/api";
 
 document.addEventListener('DOMContentLoaded', () => {
     const signupForm = document.getElementById('signup-form'); //assumes signup.html uses this ID
